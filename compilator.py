c3914fe2454a153fb5a6d7cf84666a85c7d06f66
rzx_linux <rozix.gg@gmail.com>
2023-04-02 18:33:40 +0300

    commits cleanup

---
+++ compilator.py
@@ -0,0 +1,222 @@
+filename = "1.bhtn"
+
+with open(filename) as F:
+    file = F.read()
+    file = file.replace("\n", " ")
+
+def writeRustCode(source):
+    with open("src/main.rs","w") as F:
+        F.write(source)
+        F.close()
+
+import modules as bruhtonModules
+languageWords = [
+    "Computer",
+    "work",
+    "born",
+    "do",
+    "chill",
+    "intBruh-16",
+    "intBruh-32",
+    "intBruh-64",
+    "floBruh-32",
+    "floBruh-64",
+    "Bruhing",
+    "named",
+    "think",
+    "sorry",
+    "and",
+    "or",
+    "prinput",
+    "->",
+    "<-",
+    "=>",
+    "&",
+    "bigboy",
+    "smallboy",
+    "sameboy",
+    "notsameboy",
+]
+varTypes = [
+    "intBruh-16",
+    "intBruh-32",
+    "intBruh-64",
+    "floBruh-32",
+    "floBruh-64",
+    "Bruhing",
+]
+modules = [
+    "Computer",
+]
+
+
+#used in compilation variables:
+variables = {}
+output = ""
+state = 0
+words = []
+x = ""
+
+def createVariable(type, name):
+    if type == "intBruh-16":
+        variables[name] = type
+        return f"let mut {name}: i16 = 0; "
+    elif type == "intBruh-32":
+        variables[name] = type
+        return f"let mut {name}: i32 = 0; "
+    elif type == "intBruh-64":
+        variables[name] = type
+        return f"let mut {name}: i64 = 0; "
+    elif type == "floBruh-32":
+        variables[name] = type
+        return f"let mut {name}: f32 = 0.0; "
+    elif type == "floBruh-64":
+        variables[name] = type
+        return f"let mut {name}: f64 = 0.0; "
+    elif type == "Bruhing":
+        variables[name] = type
+        return f'let mut {name}: String = "".to_string(); '
+    else:
+        printCompilationError(f"wrong variable type on createVariable step, used: {name} -> {type}")
+        return ""
+
+def createVariableWithValue(type, name, value):
+    if type == "intBruh-16":
+        variables[name] = type
+        return f"let mut {name}: i16 = {value}; "
+    elif type == "intBruh-32":
+        variables[name] = type
+        return f"let mut {name}: i32 = {value}; "
+    elif type == "intBruh-64":
+        variables[name] = type
+        return f"let mut {name}: i64 = {value}; "
+    elif type == "floBruh-32":
+        variables[name] = type
+        return f"let mut {name}: f32 = {value}; "
+    elif type == "floBruh-64":
+        variables[name] = type
+        return f"let mut {name}: f64 = {value}; "
+    elif type == "Bruhing":
+        variables[name] = type
+        return f'let mut {name}: String = "{value}".to_string(); '
+    else:
+        printCompilationError(f"wrong variable type on createVariableWithValue step, used: {name} -> {type}")
+        return ""
+
+def printCompilationError(text):
+    print(f"// Compilation Error: {text}")
+
+for i in file:
+    if state == 0:
+        if x == "" and i == " ":
+            continue
+        else:
+            state = 1
+            x += i
+ 
+    elif state == 1:
+        if i == " " or i == ";":
+            state = 0
+            words.append(x)
+            x = ""
+        else:
+            x += i
+if x != "":
+    words.append(x)
+ 
+ 
+#print(words)
+context = []
+for i in words:
+    #print(i, context)
+    if "Computer" in context:
+        if i == "work":
+            output += "fn main() { "
+            context = []
+        elif i == "chill":
+            output += "}"
+            context = []
+            state = 99
+        elif i == "born":
+            context = ["born"]
+        elif i == "do":
+            context = ["do"]
+        else:
+            printCompilationError(f"waited for function, got: {i}")
+            break
+ 
+    elif "born" in context:
+        if len(context) == 1:
+            if i in varTypes:
+                context.append(i)
+            else:
+                printCompilationError(f"waited for variable type, got: {i}")
+                break
+        elif len(context) == 2:
+            if i == "named":
+                context.append("named")
+            else:
+                printCompilationError(f'waited for keyword "named", got: {i}')
+                break
+        elif len(context) == 3:
+            if i in languageWords:
+                printCompilationError(
+                    f"use of restricted variable name, please change: {i}"
+                )
+                break
+            else:
+                context.append(i)
+        elif len(context) == 4:
+            if i == "=>":
+                context.append(i)
+            else:
+                output += createVariable(context[1], context[3])
+                if i in varTypes:
+                    context = ["born", i]
+                else:
+                    if i in modules:
+                        context = [i]
+                    else:
+                        printCompilationError(f'Waited for variable or module, check for typos or "Computer" keyword, context: {context}, got: {i}')
+                        break
+        elif len(context) == 5:
+            context.append(i)
+            output += createVariableWithValue(context[1], context[3], context[5])
+            context = ["born"]
+        else:
+            if i in modules:
+                context = [i]
+            else:
+                printCompilationError(f'Waited for variable or module, check for typos or "Computer" keyword, context: {context}, got: {i}')
+                break
+
+    elif "do" in context:
+        if len(context) == 1:
+            if bruhtonModules.getModule(i) != "":
+                context.append(i)
+            elif i in modules:
+                context = [i]
+            else:
+                printCompilationError(f"module not found, maybe there's a typo? context: {context}, got: {i}")
+                break
+        elif len(context) == 2:
+            if i in bruhtonModules.getModule(context[1]).operators:
+                context.append(i)
+            else:
+                printCompilationError(f"operator not found, module can: {bruhtonModules.getModule(context[1]).operators}, got: {i}")
+                break
+        elif len(context) == 3:
+            output += (bruhtonModules.getModule(context[1])).returnCode([context[2],i])
+            context = ["do"]
+        else:
+            printCompilationError(f"something's wrong. context: {context}, got: {i}")
+            break
+    else:
+        if i in modules:
+            context = [i]
+        else:
+            printCompilationError(f"word doesn't found in Bruhton, maybe there's a typo? context: {context}, got: {i}")
+            break
+if state == 99:
+    writeRustCode(output)
+    print('compilation done! code in "src/main.rs"')
---
+++ modules.py
@@ -0,0 +1,15 @@
+class prinput():
+    operators = ["<=", "=>"]
+    def returnCode(input):
+        if input[0] == "<=":
+            return str('print!("{}", ' + input[1] + ');')
+        elif input[0] == "=>":
+            return "!"
+        else:
+            return ""
+
+def getModule(input):
+    if input == "prinput":
+        return prinput
+    else:
+        return ""
